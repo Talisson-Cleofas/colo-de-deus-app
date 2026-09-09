0c36dba534e3617ca5c99fa296ed9cf88979867b
Talisson Cleofas <108986319+Talisson-Cleofas@users.noreply.github.com>
2026-09-09 17:49:50 -0300

    hmg: atualiza apps/api/src/cenacle-missions/cenacle-missions.module.ts

--- apps/api/src/cenacle-missions/cenacle-missions.module.ts
+++ apps/api/src/cenacle-missions/cenacle-missions.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { CenacleMissionsController } from './cenacle-missions.controller';
 import { CenacleMissionsService } from './cenacle-missions.service';
 
-@Module({ controllers: [CenacleMissionsController], providers: [CenacleMissionsService] })
+@Module({ imports: [NotificationsModule], controllers: [CenacleMissionsController], providers: [CenacleMissionsService] })
 export class CenacleMissionsModule {}
